7ecca1ee968baea4dfaf76c6a7800136f074e94d
Arthur Souza Armond <90006665+Arthur-Souza-Armond@users.noreply.github.com>
2025-02-11 18:33:27 -0300

    Revert "Fix errors flowbuilder"

--- backend/src/controllers/ContactController.ts
+++ backend/src/controllers/ContactController.ts
@@ -101,7 +101,8 @@ export const storeUpload = async (req: Request, res: Response) : Promise<Respons
   });
 
   const promises = contacts.map(async contact => {
-    const newContact : ContactData = {name: contact.Nome, number: String(contact.Telefone).replace(/\D/g, '')}
+
+    const newContact : ContactData = {name: contact.Nome, number: contact.Telefone.replace(/\D/g, '')}
 
     try{
 
