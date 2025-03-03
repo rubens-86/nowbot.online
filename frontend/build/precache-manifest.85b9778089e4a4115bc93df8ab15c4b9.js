f951dd1d9c2bc89715d4a8769681d515cd18062a
Rubens Gonçalves Júnior <rubox1000@gmail.com>
2025-03-03 20:32:21 -0300

    Adicionar configuração de servidor Express para frontend em produção

---
+++ frontend/build/precache-manifest.85b9778089e4a4115bc93df8ab15c4b9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b78501edb156a3beba7f5fc24131360",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3636ee62c99531a58692",
+    "url": "/static/css/2.cd8a6809.chunk.css"
+  },
+  {
+    "revision": "e9f8ba37f29ea0bf6b6d",
+    "url": "/static/css/main.4ab74a11.chunk.css"
+  },
+  {
+    "revision": "3636ee62c99531a58692",
+    "url": "/static/js/2.2094ec20.chunk.js"
+  },
+  {
+    "revision": "4ca9e05620d0bead510abbf71a1260a3",
+    "url": "/static/js/2.2094ec20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a3402aaf05f8c319a6d",
+    "url": "/static/js/3.89ac163b.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/static/js/3.89ac163b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f8ba37f29ea0bf6b6d",
+    "url": "/static/js/main.15da1a9c.chunk.js"
+  },
+  {
+    "revision": "e42399a1400f274c1028",
+    "url": "/static/js/runtime-main.4768d814.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "67730401e2bd45b546877cc615bc5735",
+    "url": "/static/media/logo.67730401.png"
+  },
+  {
+    "revision": "5c788b8d54d3ebf021114528116dfd77",
+    "url": "/static/media/n8n.5c788b8d.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "8244f0423cb09d847ac99e7546cb9220",
+    "url": "/static/media/wa-background-dark.8244f042.png"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  },
+  {
+    "revision": "e5374d73ff43538fdaa16a3f1a980f4b",
+    "url": "/static/media/webhook.e5374d73.png"
+  }
+]);
